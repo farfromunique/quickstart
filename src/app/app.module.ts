43353b3e19c225edcba1e66473ed6b76323adbb1
Aaron C <aaron@acwpd.com>
2017-05-31 15:03:54 -0700

    Reverting to known-good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PostCampaignReportComponent } from "./post-campaign.report.component";
 
 @NgModule({
-	imports: [BrowserModule, HttpModule],
-
-	declarations: [PostCampaignReportComponent],
-	bootstrap: [PostCampaignReportComponent]
+	imports: [BrowserModule],
+	declarations: [AppComponent],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clickthroughlink.ts
+++
@@ -1,7 +0,0 @@
-export class ClickthroughLink {
-	constructor(
-		public ct_url: string,
-		public ct_count: number,
-		public ct_u_count: number
-	) { }
-}
